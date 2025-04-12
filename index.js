5cecb720425f76327850d5bbd9a31fa4403af93c
Libor <libor.gess11@gmail.com>
2025-04-12 15:51:40 +0200

    processing data from deligated class whereHouse.js

--- index.js
+++ index.js
@@ -1,67 +1,8 @@
-// const data  = require('./data.js');
 import data from './data.js';
-class werhouse {
+import warehouse from './whereHouse.js';
 
-    constructor (data) {
-        this.data = data;
-        this.gate = '';
-        this.row = '';
-        this.sn;
-    }
-
-    search = (sn) => {
-
-        let result = '';
-        const data = this.data;
-
-
-        result = data.filter((num => num.sn === sn));
-        this.sn = result[0].sn;
-        this.gete = result[0].gate;
-
-       return result.length > 0 ? result : "Sorry";
-    }
-
-    getGate = (sn) => {
-
-        let vehicle;
-
-        vehicle = this.data.filter((num => num.sn === sn));
-
-        const {gate, row, position} = vehicle[0];
-      
-       
-        return this.gate = this.data.filter((vehicle) => vehicle.gate === gate);
-        
-    }
-
-    getFuealLevel = () => {
-
-        let vehicle;
-
-        return vehicle = this.data.filter((vehicle) => 
-            (typeof vehicle.fuelLevel === 'number' && vehicle.fuelLevel < 45 || vehicle.fuelLevel === "low")
-        );
-
-    }
-
-    // Get all vehicles in a row that gate is = to by giveen gate by user
-    getRowsByGate = (something) => {
-        let vehicle;
-
-        vehicle = this.data.filter((vehicle) => vehicle.gate === something);
-
-        return vehicle.length >=1 ? vehicle : "Sorry, the giveen gate number does not exist!, please try again";
-
-    }
-}
-
-const t = new werhouse(data);
-// console.log(t.search('DFG00028'));
-
-// console.log(t.getGate('DFG00028'));
-
-console.log(t.getRowsByGate(3));
-console.log(t.row)
-// console.log(t.row)
-
+const vehicle = new warehouse(data);
+const sn = "TAO00001";
+console.log(vehicle.search(sn));
+console.log(vehicle.getRowsByGate());
+// console.log(vehicle.row)
