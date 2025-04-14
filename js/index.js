5880d5307909eeb5d6dbb72729646bd6fe81d409
Libor <libor.gess11@gmail.com>
2025-04-14 18:11:43 +0200

    remove extra variables

--- js/index.js
+++ js/index.js
@@ -4,5 +4,10 @@ import warehouse from './whereHouse.js';
 const vehicle = new warehouse(data);
 const sn = "TAO00001";
 console.log(vehicle.search(sn));
-console.log(vehicle.getRowsByGate());
+// console.log(vehicle.getRowsByGate());
+// console.log(vehicle.getFuealLevel());
+// console.log(vehicle.getCoolantLevel());
+// console.log(vehicle.getRecovery());
+console.log(vehicle.getIssues());
+
 // console.log(vehicle.row)
