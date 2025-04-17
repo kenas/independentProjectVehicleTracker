9524adb5ea1bd694c7dc9a707e684e3d941a8780
Libor <libor.gess11@gmail.com>
2025-04-17 07:21:01 +0200

    Debug process

--- js/index.js
+++ js/index.js
@@ -3,11 +3,18 @@ import warehouse from './whereHouse.js';
 
 const vehicle = new warehouse(data);
 const sn = "TAO00001";
-console.log(vehicle.search(sn));
+// console.log(vehicle.search(sn));
 // console.log(vehicle.getRowsByGate());
 // console.log(vehicle.getFuealLevel());
 // console.log(vehicle.getCoolantLevel());
-// console.log(vehicle.getRecovery());
-console.log(vehicle.getIssues());
+// console.log(vehicle.getRecovery());\
+// console.log(vehicle.getAllUIC());
+// console.log(vehicle.category);
+// console.log(vehicle.getVehiclesByCatgotyUIC());
+
+
+const container = document.getElementById("vehicle-list");
+console.log(container);
+
 
 // console.log(vehicle.row)
